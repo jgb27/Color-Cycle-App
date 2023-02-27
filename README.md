db13d747d869851cc3306889e662ab993e9b500d
João Gustavo Soares Bispo <jgbispo20@gmail.com>
2023-02-27 10:48:57 -0300

    Change colo ready

--- README.md
+++ README.md
@@ -11,23 +11,3 @@ over time also based on user input. In other words, from cycles through
 changes to the originally specified color. These changes allow the user to
 experience the visual impact different changes to the individual parts of
 an RGB color specification (e.g. `#000000` color code).
-
-## User Stories
-
--   [ ] User can specify a starting fill color as a six hexadecimal standard
-        CSS color code in three individual components of two digits each - red,
-        blue, and green
--   [ ] User can specify an increment value for each color component that will
-        be added to that component every .25 second
--   [ ] User can see the box containing the fill color change every .25 seconds
--   [ ] User can only change the color components and their increments when
-        the app is stopped
--   [ ] User can start and stop the fill operation using a button whose name
-        changes to 'Start' when stopped and 'Stop' when started
--   [ ] User will receive an warning if something other than hexadecimal digits
-        are entered for the color components
-
-## Bonus features
-
--   [ ] User can change the time interval between color changes
--   [ ] User can specify the color encoding format used from RGB to another format like HSL
--- script.js
+++ script.js
@@ -1,14 +1,63 @@
 const start = document.querySelector('#start');
 const stop = document.querySelector('#stop');
+const spanError = document.querySelector('#error');
 
-
-const changeColor = () => {
-    const colorValue = document.querySelector('#color').value;
-    document.body.style.backgroundColor = `#${colorValue}`;
+// verify if the input is a valid hex color
+const isHexColor = (color) => {
+    const regex = /^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/;
+    return regex.test(color);
 }
 
-start.addEventListener('click', changeColor);
+// change the background color
+const changeColor = (color) => {
+    document.body.style.backgroundColor = color;
+}
+
+// get the input value and verify if it's a valid color
+const getColor = () => {
+    const color = document.querySelector('#color').value;
+    
+    if (isHexColor(`#${color}`)) {
+        loopThroughHexColors(color);
+        spanError.textContent = '';
+    } else {
+        spanError.textContent = 'Please enter a valid hex color';
+    }
+}
+
+
+// loop through the hex colors
+const loopThroughHexColors = (color) => {
+    const interval = setInterval(() => {
+        if (color === 'ffffff') {
+            clearInterval(interval);
+        } else {
+            color = nextHexColor(color);
+            changeColor(`#${color}`);
+        }
+    }, 0.25);
+}
+
+
+// next hex color
+const nextHexColor = (color) => {
+    const hex = parseInt(color, 16);
+    const nextHex = hex + 1;
+    const nextHexColor = nextHex.toString(16);
+
+    if(nextHexColor.length < 6) {
+        return nextHexColor.padStart(6, '0');
+    }
+    console.log(`#${nextHexColor}`);
+    return nextHexColor;
+}
+
+// switch color slowl
+
+start.addEventListener('click', () => {
+    getColor();
+});
 
 stop.addEventListener('click', () => {
-    document.body.style.backgroundColor = '#f5f5f5';
+    location.reload();
 });
